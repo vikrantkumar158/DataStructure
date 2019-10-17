ad37c3c50619556c34b2abe329b7555b40c24f78
“vikrantkumar158” <vikrant171891.cse@chitkara.edu.in>
2019-10-18 02:55:29 +0530

    Codes

--- vikrant/ds/BinarySearch.java
+++ vikrant/ds/BinarySearch.java
@@ -106,4 +106,51 @@ public class BinarySearch
 		mid=(l+r)/2;
 		return arr[mid]+1;
 	}
+	private static int RotationCount(int arr[],int l,int r)
+	{
+		if(r<l) 
+			return 0;
+		if(r==l) 
+			return l; 
+		int mid=(l+r)/2;
+		if(mid<r&&arr[mid+1]<arr[mid])
+			return (mid+1);
+		if(mid>l&&arr[mid]<arr[mid-1])
+			return mid;
+		if (arr[r]>arr[mid]) 
+			return RotationCount(arr,l,mid-1); 
+		return RotationCount(arr,mid+1,r); 
+	}
+	public static int countRotations(int arr[])
+	{
+		return RotationCount(arr,0,arr.length-1);
+	}
+	public static int RotatedSearch(int arr[],int num,int low,int high)
+	{
+		if(low>high)
+			return -1;
+		int mid=(low+high)/2;
+		if(arr[mid]==num)
+			return mid;
+		if(arr[low]<=arr[mid])
+		{
+			if(num>=arr[low]&&num<=arr[mid])
+				return RotatedSearch(arr,num,low,mid-1);
+			return RotatedSearch(arr,num,mid+1,high);
+		}
+		if(num>=arr[mid]&&num<=arr[high])
+			return RotatedSearch(arr,num,mid+1,high);	
+		return RotatedSearch(arr,num,low,mid-1);
+	}
+	public static int search(int arr[],int num)
+	{
+		if(countRotations(arr)==0)
+		{
+			return RecursiveSearch(arr,num,0,arr.length-1);
+		}
+		else
+		{
+			return RotatedSearch(arr,num,0,arr.length);
+		}
+	}
 }
